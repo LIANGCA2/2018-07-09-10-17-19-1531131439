454053c92e2e37554a0b1f337254979083592e68
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-10 15:56:26 +0800

    finifsh p11

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,46 @@
 package practice10;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private ArrayList memberList = new ArrayList<Student>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String  getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student) {
+        if(student.getKlass().getNumber()==getNumber()){
+            leader = student;
+        }else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+
+    public void appendMember(Student student) {
+        memberList.add(student);
+    }
 }
