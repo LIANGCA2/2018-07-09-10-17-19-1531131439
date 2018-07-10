60ad6b13c3eb301ac782d90a7a013720f00c4cac
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-10 14:02:24 +0800

    finish p3

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,35 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+
+    private  String name;
+    private Integer age;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
