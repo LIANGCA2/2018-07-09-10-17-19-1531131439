26852262c772cf661fa7483a820a3629845500c6
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-10 14:13:27 +0800

    finnish p4-p5

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,36 @@
 package practice05;
 
+
 public class Person {
+    private String name;
+    private Integer age;
+    public Person(){
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String basicIntroduce() {
+        return "My name is "+getName()+". I am "+getAge()+" years old.";
+    }
+    public String introduce() {
+        return basicIntroduce();
+    }
 }
--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,23 @@
 package practice05;
 
-public class Student {
+public class Student  extends Person{
+    private Integer klass;
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+getKlass()+".";
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
 }
