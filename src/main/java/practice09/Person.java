6bbb9265eae22c8fbdef009d39074b1ea999b8cd
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-10 15:27:21 +0800

    finnish p9

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,59 @@
 package practice09;
 
+
 public class Person {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String name;
+    private Integer age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String basicIntroduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old.";
+    }
+
+    public String introduce() {
+        return basicIntroduce();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        return obj!=null && getName().equals(((Person) obj).getName()) && getAge().equals(((Person) obj).getAge()) && getId().equals(((Person) obj).getId());
+    }
 }
