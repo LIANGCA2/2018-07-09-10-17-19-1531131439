8b752a119a7eda828b3de21d3a43645690eeb655
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-10 14:25:32 +0800

    finifsh p5 and p6

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,23 @@
 package practice06;
+public class Student  extends Person {
+    private Integer klass;
 
-public class Student {
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+getKlass()+".";
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
 }
+
