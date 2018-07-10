a3270129862f5f8d0b0559a59b56c596e4588468
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-10 16:45:48 +0800

    finnish p12

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,73 @@
 package practice11;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private ArrayList memberList = new ArrayList<Student>();
+    private ArrayList<Teacher> assignLeaderListenerList = new ArrayList<>();
+    private ArrayList<Teacher> joinClassListener = new ArrayList<>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String  getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student) {
+        if(student.getKlass().getNumber()==getNumber()){
+            leader = student;
+            notifyTeacherSomeoneAssignLeader(student);
+        }else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void notifyTeacherSomeoneAssignLeader(Student student) {
+        for(int i = 0;i<assignLeaderListenerList.size();i++){
+            assignLeaderListenerList.get(i).sayAssignLeader(student,this.number);
+          //  (assignLeaderListenerList.get(i)).sayAssignLeader(student,this.number);
+        }
+    }
+
+
+    public void notifyTeacherSomeoneJoinClass(Student student) {
+        for(int i = 0;i<joinClassListener.size();i++){
+            joinClassListener.get(i).saySomeoneJoinClass(student,this.number);
+            //  (assignLeaderListenerList.get(i)).sayAssignLeader(student,this.number);
+        }
+    }
+
+    public void registerJoinListener(Teacher teacher){
+        joinClassListener.add(teacher);
+    }
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        memberList.add(student);
+        notifyTeacherSomeoneJoinClass(student);
+    }
+
+    public void registerAssignLeaderListener(Teacher teacher){
+        assignLeaderListenerList.add(teacher);
+    }
+
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,67 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> classes = new LinkedList<>();
+
+    public Teacher(int id, String name, int age) {
+        super(id,name,age);
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(LinkedList<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+        super(id,name,age);
+        this.classes = klasses;
+        for(int i = 0;i < classes.size();i++){
+            classes.get(i).registerAssignLeaderListener(this);
+            classes.get(i).registerJoinListener(this);
+        }
+    }
+
+   public void sayAssignLeader(Student student,int number){
+       System.out.print("I am "+getName()+". I know "+student.getName()+" become Leader of Class "+number+".\n");
+   }
+
+
+    @Override
+    public String introduce() {
+        if(getClasses().size() == 0){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }else{
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i< classes.size(); i++){
+                sb.append(classes.get(i).getNumber()+", ");
+            }
+            return super.introduce()+" I am a Teacher. I teach Class "+sb.substring(0,sb.length()-2)+".";
+        }
+
+    }
+
+    public String introduceWith(Student student) {
+        if(!getClasses().contains(student.getKlass())){
+            return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+    }
+
+    public boolean isTeaching(Student student) {
+        if(getClasses().contains(student.getKlass())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void saySomeoneJoinClass(Student student, Integer number) {
+        System.out.print("I am "+getName()+". I know "+student.getName()+" has joined Class "+number+".\n");
+    }
 }
